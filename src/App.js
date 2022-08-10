ecb3e8b5f594909df7bd1c3ff6c66433283f3ace
20201206-013040\Administrator <xingbangl@gmail.com>
2022-08-10 23:46:08 +0800

    study

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Test from "./Test"
 
-function App() {
+function App () {
   return (
     <div className="App">
+      <Test />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
